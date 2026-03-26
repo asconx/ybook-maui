3f77d8292cc5791c9d7c628bc21dd86f01531e71
DESKTOP-OHB19E3\admin <anna@nowocin.pl>
2026-03-26 14:05:11 +0100

    ankiety

--- yBook/MauiProgram.cs
+++ yBook/MauiProgram.cs
@@ -2,6 +2,7 @@ using Microsoft.Extensions.Logging;
 using yBook.Services;
 using yBook.ViewModels;
 using yBook.Views.Auth;
+using yBook.Views.Surveys;
 
 namespace yBook
 {
@@ -23,17 +24,21 @@ namespace yBook
 
             // ── Services (Singleton — żyją przez cały czas działania aplikacji)
             builder.Services.AddSingleton<IAuthService, AuthService>();
-
+            builder.Services.AddSingleton<ISurveyService, SurveyService>();
 
             // ── Dodaj tę linię:
             builder.Services.AddSingleton<AppShell>();
 
             // ── ViewModels (Transient — nowa instancja przy każdej nawigacji)
             builder.Services.AddTransient<LoginViewModel>();
+            builder.Services.AddTransient<SurveysViewModel>();
+            builder.Services.AddTransient<EditSurveyViewModel>();
 
             // ── Pages ─────────────────────────────────────────────────────────
             builder.Services.AddTransient<LoginPage>();
             builder.Services.AddTransient<MainPage>();
+            builder.Services.AddTransient<SurveysPage>();
+            builder.Services.AddTransient<EditSurveyPage>();
 
 #if DEBUG
             builder.Logging.AddDebug();
