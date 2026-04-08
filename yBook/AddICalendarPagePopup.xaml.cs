9e581ec36b018251b58db04718a818f6961f71a3
DESKTOP-6RJR67T\4TP <annna@nowocin.pl>
2026-04-08 11:13:14 +0200

    Dodanie Popup (Nie działa jeszcze prosze nie mergować by nie psuć innym działania aplkiacji)

---
+++ yBook/AddICalendarPagePopup.xaml.cs
@@ -0,0 +1,61 @@
+namespace yBook.Views.ICalendar
+{
+    public partial class AddCalendarPopup : ContentPage
+    {
+        public TaskCompletionSource<CalendarItem> tcs = new();
+
+        private CalendarItem editingItem;
+
+        public AddCalendarPopup(CalendarItem item = null)
+        {
+            InitializeComponent();
+
+            if (item != null)
+            {
+                editingItem = item;
+
+                NameEntry.Text = item.Name;
+                ExportLinkEntry.Text = item.ExportLink;
+                Import1Entry.Text = item.Import1;
+                Import2Entry.Text = item.Import2;
+                ExtraEntry.Text = item.Extra;
+            }
+        }
+
+        private void OnNameChanged(object sender, TextChangedEventArgs e)
+        {
+            var name = e.NewTextValue;
+
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                ExportLinkEntry.Text = string.Empty;
+                return;
+            }
+
+            var safeName = name.Replace(" ", "_").ToLower();
+
+            ExportLinkEntry.Text = $"https://api.ybook.com/export/{safeName}_{Guid.NewGuid()}";
+        }
+
+        private async void OnSaveClicked(object sender, EventArgs e)
+        {
+            var item = editingItem ?? new CalendarItem();
+
+            item.Name = NameEntry.Text;
+            item.ExportLink = ExportLinkEntry.Text;
+            item.Import1 = Import1Entry.Text;
+            item.Import2 = Import2Entry.Text;
+            item.Extra = ExtraEntry.Text;
+
+            tcs.SetResult(item);
+
+            await Navigation.PopModalAsync();
+        }
+
+        private async void OnCancelClicked(object sender, EventArgs e)
+        {
+            tcs.SetResult(null);
+            await Navigation.PopModalAsync();
+        }
+    }
+}
--- yBook/ICalendarPage.xaml.cs
+++ yBook/ICalendarPage.xaml.cs
@@ -1,22 +1,75 @@
+using System.Collections.ObjectModel;
 using System.Reflection.PortableExecutable;
 using yBook.Models;
+using static Android.Preferences.PreferenceActivity;
 
 namespace yBook.Views.ICalendar
 {
     public partial class ICalendarPage : ContentPage
     {
-       
+        public ObservableCollection<CalendarItem> Items { get; set; } = new();
 
         public ICalendarPage()
         {
             InitializeComponent();
             Header.HamburgerClicked += (_, _) => Drawer.Open();
+
+            BindingContext = this;
         }
 
-       public void OnDodajClicked(object sender, EventArgs e) 
+        public async void OnDodajClicked(object sender, EventArgs e)
         {
+            var popup = new AddCalendarPopup();
 
+            await Navigation.PushModalAsync(popup);
+
+            var result = await popup.tcs.Task;
+
+            if (result != null)
+            {
+                Items.Add(result);
+                LblCount.Text = Items.Count.ToString();
+            }
+        }
+
+        private async void OnEditClicked(object sender, EventArgs e)
+        {
+            var button = sender as Button;
+            var item = button?.BindingContext as CalendarItem;
+
+            if (item == null) return;
+
+            var popup = new AddCalendarPopup(item);
+
+            await Navigation.PushModalAsync(popup);
+
+            var result = await popup.tcs.Task;
+
+            if (result != null)
+            {
+                var index = Items.IndexOf(item);
+                Items[index] = result;
+            }
+        }
+
+        private void OnDeleteClicked(object sender, EventArgs e)
+        {
+            var button = sender as Button;
+            var item = button?.BindingContext as CalendarItem;
+
+            if (item == null) return;
+
+            Items.Remove(item);
+            LblCount.Text = Items.Count.ToString();
         }
     }
 
+    public class CalendarItem
+    {
+        public string Name { get; set; }
+        public string ExportLink { get; set; }
+        public string Import1 { get; set; }
+        public string Import2 { get; set; }
+        public string Extra { get; set; }
+    }
 }
