bcb7cec0bd922111a8d60a1f6acc65aa9ca19b4f
DESKTOP-0HNVK46\4TP Holoshchuk <edholoshchuk07@gmail.com>
2026-04-08 09:36:29 +0200

    room

---
+++ yBook/Views/Ustawienia/Nowy folder/DodajPokojePage.xaml.cs
@@ -0,0 +1,12 @@
+using Microsoft.Maui.Controls;
+
+namespace yBook.Views.Ustawienia
+{
+    public partial class DodajPokojePage : ContentPage
+    {
+        public DodajPokojePage()
+        {
+            InitializeComponent();
+        }
+    }
+}
